8dd17623f1914782044c62f683c2e32a39f5c324
Qian Zhou <qzhou@stuy.edu>
2018-01-11 08:24:47 -0500

    change Item

--- Item.java
+++ Item.java
@@ -1,3 +1,30 @@
 public class  Item{
+    // ----------instance variables-------------
+    private int deliveryTime, cost, healthVal, price;
+    //           to determine info in shop and inventories, health val is effect on thirst/hunger determined by subclasses; price is selleable value
+    private String name;
 
+    // --------------methods -----------------
+    public Item(){
+    }
+    public String toString(){
+	String retVal = "";
+	return retVal;
+    }
+    //accessors
+    
+    public int getDelTime(){
+	return deliveryTime;
+    }
+    
+    public int getCost(){
+	return cost;
+    }
+
+    //mutators
+    public int changeDelTime(int change){// to be used for user to change deliveryTime or shop manage items
+	int i = deliveryTime;
+	deliveryTime+= change;
+	return i; // return old value 
 }
+    public
