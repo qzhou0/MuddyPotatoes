dc0ee89b06007e91805c67d2611e2a245398c66e
taniacao12 <33487802+taniacao12@users.noreply.github.com>
2018-01-13 00:21:02 -0500

    Update Woo.java

--- Woo.java
+++ Woo.java
@@ -1,9 +1,76 @@
+import java.io.*;
+import java.util.*;
+
 public class Woo {
     // instance variables
-    private String user;
-    private int balance, hunger, thirst;
+    public final static int MAX_DURATION = 10;
+    private int day, balance, hunger, thirst, gameOver;
     private ArrayList<Item> inventory;
+    private InputStreamReader isr;
+    private BufferedReader in;
 
     // default constructor
-    
+    public Woo() {
+	day = 0;
+	balance = Math.random() * 101 + 100;
+	hunger = 80;
+	thirst = 80;
+	gameOver = false;
+    }
+
+    public static int locate (Item item) {
+	
+    }
+
+    public static void buy (Item item) {
+
+    }
+
+    public static void sell (Item item) {
+
+    }
+
+    public static String getValues() {
+	String  s = "Day: " + day;
+	s += "\nBalance: " + balance;
+	s += "\nHunger Lvl: " + hunger;
+	s += "\nThirst Lvl: " + thirst;
+	return s;
+    }
+    /*
+    public static String getDrinks() {
+	String s = "";
+	// insert code here
+    }
+
+    public static String getFoods() {
+	String s = "";
+	// insert code here
+    }
+    */
+    public static String getUnconsumables() {
+	String s = "";
+	// insert code here
+    }
+
+    public static void run(){
+	if (day > 10){
+	    gameOver = 2; // option 2: You won the game
+	}
+	else {
+	    
+	}
+    }
+	
+    public static void main (String[] args) {
+	Woo test = new Woo();
+
+        while (gameOver == 0) {
+	    Woo.run();
+	}
+
+	if (day <= 10) {
+	    System.out.println("GAME OVER
+	    
+    }
 }
