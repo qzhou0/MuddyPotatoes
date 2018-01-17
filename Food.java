d76158b96b9e3689823e1de6d3b2e6f55810cbf6
Tania Cao <tcaosu@stuy.edu>
2018-01-16 20:04:06 -0500

    Update Food.java

--- Food.java
+++ Food.java
@@ -4,15 +4,13 @@ public class Food extends Item{
     }
     // should we add an overloaded constructor?
     
-    //should be similar for Drink, except the type becomes Drink, and value added to "Hunger" becomes "Thirst"
     public String toString(){
-      String s ="";
+      String s = "";
       s += super.toString();// will this work?
       s += "\nType: Food";
       s += "\nNutrition Value: " + foodVal;
       s += "\nHydration Value: " + drinkVal;
       return s;
     }
-    
 }
 
