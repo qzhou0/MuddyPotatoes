bb60d3852127d9f068078c11bc3490fb1e616a1f
Qian Zhou <qzhou@stuy.edu>
2018-01-16 20:29:09 -0500

    update

--- Food.java
+++ Food.java
@@ -1,27 +1,19 @@
 public class Food extends Item{
     public Food(){
-<<<<<<< HEAD
 	price = 0;//Food cannot sell
 	drinkVal = 0;
-=======
-	price = 0; //Food cannot sell
->>>>>>> 61c3d0f22d2ccc8f505a3e411057794e43bf9e02
     }
     // should we add an overloaded constructor?
     
+    //should be similar for Drink, except the type becomes Drink, and value added to "Hunger" becomes "Thirst"
     public String toString(){
-      String s = "";
+      String s ="";
       s += super.toString();// will this work?
-<<<<<<< HEAD
       s += "\tType: Food";
       s += "\tValue added to hunger:" + foodVal;
       s += "\n";
-=======
-      s += "\nType: Food";
-      s += "\nNutrition Value: " + foodVal;
-      s += "\nHydration Value: " + drinkVal;
->>>>>>> 61c3d0f22d2ccc8f505a3e411057794e43bf9e02
       return s;
     }
+
 }
 
--- Nonconsumable.java
+++ Nonconsumable.java
@@ -1,21 +1,18 @@
 public class Nonconsumable extends Item{
+
     public Nonconsumable(){
-<<<<<<< HEAD
 	drinkVal = 0;
 	foodVal = 0;
 	//healthVal = 0;
-=======
-	// is this necessary?
-	NutritionVal = 0;
-	HydrationVal = 0;
->>>>>>> 61c3d0f22d2ccc8f505a3e411057794e43bf9e02
     }
 
     public String toString(){
-	String s = "";
-      s += super.toString(); // will this work?
+      String s ="";
+      s += super.toString();// will this work?
       s += "\nType: Nonconsumable";
-      s += "\nSelling Price:" + price;
+      s += "\nPrice that can be sold at:" + price;
+      s += "\n";
       return s;
     }
 }
+
